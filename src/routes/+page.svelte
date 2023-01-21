a82789c38e9be37bfd95a47a8cf624cff82de460
Fernando Maclen <fmaclen@gmail.com>
2023-01-20 22:55:48 -0500

    feat: add basic design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,214 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<h1 class="logo">The Synthetic Gazette</h1>
+
+	<main class="homepage">
+		<article class="homepage__article">
+			<h1 class="homepage__h1">Future of Lawn Furniture: Smart and Connected</h1>
+			<h2 class="homepage__h2">How technology is revolutionizing outdoor living spaces</h2>
+			<p class="homepage__p">
+				Gone are the days of simple, static outdoor furniture. Today, smart and connected lawn
+				furniture is the way of the future. With the integration of technology, these new designs
+				offer homeowners a wide range of features and benefits that were once unimaginable.
+			</p>
+			<p class="homepage__p">
+				One of the most notable features of smart lawn furniture is the ability to control and
+				customize your outdoor living space through a mobile app or voice commands. This can include
+				adjusting the lighting, setting temperature controls, and even playing music. In addition,
+				some designs come with built-in sensors that can detect the weather, and automatically
+				adjust to provide optimal comfort for the conditions.
+			</p>
+			<p class="homepage__p">
+				Another advantage of smart lawn furniture is the ability to connect to other smart devices
+				in your home, such as security cameras, thermostats, and lighting systems. This allows for
+				even more control and customization of your outdoor living space, as well as added
+				convenience and security.
+			</p>
+			<p class="homepage__p">
+				Smart lawn furniture is also becoming more sustainable, with designs that incorporate solar
+				panels and rainwater harvesting systems. This not only helps to reduce energy consumption
+				and water usage, but also helps to reduce the environmental impact of outdoor living spaces.
+			</p>
+			<p class="homepage__p">
+				Overall, smart and connected lawn furniture is the future of outdoor living spaces. With the
+				integration of technology, these new designs offer homeowners a wide range of features and
+				benefits that were once unimaginable, while also promoting sustainability.
+			</p>
+		</article>
+
+		<article class="homepage__article">
+			<h1 class="homepage__h1">The Elbonian Revolution</h1>
+			<h2 class="homepage__h2">
+				The people of Elbonia rise up against their oppressive government
+			</h2>
+			<p class="homepage__p">
+				The small country of Elbonia, located in the fictional region of the world, has been rocked
+				by a massive popular uprising against its long-time authoritarian government. For years, the
+				people of Elbonia have suffered under a regime that has suppressed basic human rights and
+				stifled economic growth. However, in recent months, a growing movement has emerged,
+				demanding change and an end to the status quo.
+			</p>
+		</article>
+
+		<article class="homepage__article">
+			<h1 class="homepage__h1">MH370 Mystery Finally Solved</h1>
+			<h2 class="homepage__h2">
+				After years of speculation and searching, evidence confirms the fate of the missing Malaysia
+				Airlines flight
+			</h2>
+			<p class="homepage__p">
+				On March 8, 2014, Malaysia Airlines flight MH370 disappeared without a trace, leaving the
+				world in shock and the fate of the 239 passengers and crew members unknown. For years,
+				search teams scoured the Indian Ocean in an effort to find the missing plane, and theories
+				about what happened to it ran rampant. However, recent evidence has finally provided closure
+				to this aviation mystery.
+			</p>
+			<p class="homepage__p">
+				In July 2018, a piece of debris from the plane washed up on the coast of Madagascar, and
+				further analysis confirmed it was from MH370. In addition, the government of Malaysia, in
+				conjunction with the Australian Transport Safety Bureau and the Chinese government, released
+				a report stating that the plane's final resting place is in the southern Indian Ocean.
+			</p>
+		</article>
+
+		<article class="homepage__article">
+			<h1 class="homepage__h1">Raising the Debt Ceiling: A Necessary Move for Xandaria</h1>
+			<h2 class="homepage__h2">
+				Xandaria, facing economic challenges, had to increase its debt ceiling to avoid default and
+				stabilize its economy.
+			</h2>
+			<p class="homepage__p">
+				In recent years, Xandaria has been facing economic challenges, including a slow growth in
+				GDP and high unemployment rates. As a result, the government has had to increase its
+				spending in order to stimulate the economy and support social welfare programs. However,
+				this increased spending has led to a rise in the country's debt levels, and it became
+				necessary for the government to raise the debt ceiling in order to avoid default and
+				stabilize its economy.
+			</p>
+			<p class="homepage__p">
+				Raising the debt ceiling is a controversial move, as it allows the government to borrow more
+				money, which can lead to concerns about long-term sustainability and potential inflation.
+				However, in the short-term, it was necessary for Xandaria to take this step in order to
+				avoid a financial crisis and support economic growth.
+			</p>
+			<p class="homepage__p">
+				The government has also implemented several economic reforms and austerity measures to
+				address the issues and get the economy back on track. The government has also emphasized
+				that it will focus on long-term solutions such as increasing revenue and reducing spending
+				to bring down the debt level and avoid future raises in debt ceiling.
+			</p>
+		</article>
+
+		<article class="homepage__article">
+			<h1 class="homepage__h1">The Importance of Walkable Cities</h1>
+			<h2 class="homepage__h2">Why walkability should be a key consideration in urban planning</h2>
+			<p class="homepage__p">
+				One of the key economic benefits of walkable cities is that they tend to have higher
+				property values and a more vibrant local economy. This is because walkable neighborhoods are
+				often more desirable places to live, work, and shop, which attracts more businesses and
+				people to the area.
+			</p>
+			<p class="homepage__p">
+				Despite these benefits, many cities around the world are still designed primarily for cars,
+				with wide roads and large parking lots. However, an increasing number of urban planners,
+				architects, and policymakers are beginning to recognize the importance of walkability and
+				are taking steps to make their cities more pedestrian-friendly. This includes things like
+				creating more bike lanes, building more sidewalks and crosswalks, and encouraging the
+				development of mixed-use neighborhoods. By making our cities more walkable, we can not only
+				improve our own health and well-being, but also create more vibrant and sustainable
+				communities for everyone.
+			</p>
+		</article>
+
+		<article class="homepage__article">
+			<h1 class="homepage__h1">Rising of Crypto Betting</h1>
+			<h2 class="homepage__h2">How blockchain technology is changing the way we gamble</h2>
+			<p class="homepage__p">
+				Crypto betting, also known as blockchain betting, refers to the use of cryptocurrencies,
+				such as Bitcoin and Ethereum, to place bets on various games and events. This type of
+				betting has become increasingly popular in recent years, thanks to the many benefits that it
+				offers over traditional online gambling.
+			</p>
+			<p class="homepage__p">
+				One of the biggest benefits of crypto betting is the increased level of security and
+				transparency. Because all transactions are recorded on a public blockchain, it is almost
+				impossible for anyone to cheat or manipulate the system. In addition, because
+				cryptocurrencies are decentralized, there is no need for a middleman, such as a bank, to
+				process transactions, which can also help to reduce costs and increase speed.
+			</p>
+			<p class="homepage__p">
+				Despite these benefits, crypto betting is still in its early stages and there are some
+				challenges that need to be addressed. One of these challenges is the volatility of
+				cryptocurrencies, which can make it difficult for players to know how much they are actually
+				risking. Additionally, because crypto betting is not yet regulated in many jurisdictions,
+				there is a risk that players may not be protected in case of fraud or other issues. However,
+				as the technology and regulations continue to evolve, it is likely that these challenges
+				will be overcome, and crypto betting will become an increasingly popular and mainstream way
+				to gamble.
+			</p>
+		</article>
+
+		<!-- <article class="homepage__article">
+		<h1 class="homepage__h1">Title</h1>
+		<h2 class="homepage__h2">32-word subtitle</h2>
+		<p class="homepage__p">Paragraph</p>
+		<p class="homepage__p">Short Paragraph</p>
+		<p class="homepage__p">Paragraph</p>
+		<p class="homepage__p">Paragraph</p>
+	</article> -->
+	</main>
+</div>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,700;1,400&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+	div.container {
+		max-width: 1440px;
+		margin-inline: auto;
+	}
+
+	h1.logo {
+		font-family: 'Bebas Neue', sans-serif;
+		text-align: center;
+		letter-spacing: 0.2em;
+		font-size: 1.5rem;
+		margin: 6rem 0;
+	}
+
+	main.homepage {
+		margin-top: calc(calc(var(--section-spacing) + 3rem) * -1);
+		column-count: 3;
+		break-inside: avoid-column;
+		font-family: 'EB Garamond', 'Georgia', serif;
+	}
+
+	article.homepage__article {
+		display: inline-flex;
+		flex-direction: column;
+		row-gap: 24px;
+		border: 1px solid #e2e2e2;
+		padding: 2.5rem;
+		margin-bottom: 1rem;
+	}
+
+	h1.homepage__h1 {
+		font-size: 2.25rem;
+		line-height: 1em;
+		margin: 0;
+	}
+
+	h2.homepage__h2 {
+		font-style: italic;
+		font-size: 1.5rem;
+		line-height: 1.15em;
+		margin: 0;
+	}
+
+	p.homepage__p {
+		margin: 0;
+
+		&:first-of-type {
+			font-size: 1.25em;
+		}
+	}
+</style>
