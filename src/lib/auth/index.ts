0f87cd952456369e500d3f5bf7bcd89b4845aab4
Fernando Maclen <fmaclen@gmail.com>
2023-01-22 17:16:20 -0500

    chore: remove unused files

--- src/lib/auth/index.ts
+++
@@ -1,6 +0,0 @@
-// /**
-//  * Uncomment the auth adapter you'd like to use.
-//  * Please visit the readme for more information on how to use the adapters.
-//  */
-// export { cookie as auth } from './cookie';
-// // export { pocketbase as auth } from "./pocketbase";
