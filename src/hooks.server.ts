6e712fb8c3a5b7c33029ab72916a2f83e8029fa2
Fernando Maclen <fmaclen@gmail.com>
2023-01-29 11:12:08 -0500

    fix: authentication and protected routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,16 @@
-import { pb, serializeNonPOJOs } from '$lib/pocketbase.server';
+import { pocketbaseURL, serializeNonPOJOs } from '$lib/pocketbase.server';
 import type { Handle, RequestEvent } from '@sveltejs/kit';
+import pocketbaseEs from 'pocketbase';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	await handleUserAuth(event);
 
 	// Handle protected paths
-	const PROTECTED_PATHS = ['/play'];
-	if (PROTECTED_PATHS.some((path) => event.url.pathname.includes(path))) {
-		return Response.redirect(`${event.url.origin}/login`, 303);
+	if (!event.locals.user) {
+		const PROTECTED_PATHS = ['/play'];
+		if (PROTECTED_PATHS.some((path) => event.url.pathname.includes(path))) {
+			return Response.redirect(`${event.url.origin}/login`, 303);
+		}
 	}
 
 	// Set cookies
@@ -18,7 +21,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 };
 
 const handleUserAuth = async (event: RequestEvent) => {
-	event.locals.pb = pb;
+	event.locals.pb = new pocketbaseEs(pocketbaseURL);
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
 	try {
