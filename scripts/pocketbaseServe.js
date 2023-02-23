d41c32de1db8b13c97948bd8ac71a7b3ab137ca4
Fernando Maclen <fmaclen@gmail.com>
2023-02-23 11:35:14 -0500

    test: fix pocketbaseServe.js so it runs on Windows

--- scripts/pocketbaseServe.js
+++ scripts/pocketbaseServe.js
@@ -1,29 +1,34 @@
 import { spawn } from 'child_process';
+import path from 'path';
 
 function runPocketBaseServe() {
 	const args = process.argv.slice(2); // Ignore first two arguments
 	const port = args[0] ? parseInt(args[0]) : 8090; // Default port is 8090
-	const fileExtension = process.platform === 'win32' ? '.exe' : '';
 
 	let http = `127.0.0.1:${port}`;
 	let dir, migrationsDir;
 
 	// If port is 8091, use the `tests/pocketbase` directories for `pb_data` and `pb_migrations`
 	if (port === 8091) {
-		dir = 'tests/pocketbase/pb_data';
-		migrationsDir = 'tests/pocketbase/pb_migrations';
+		dir = path.join('tests', 'pocketbase', 'pb_data');
+		migrationsDir = path.join('tests', 'pocketbase', 'pb_migrations');
 	}
 
-	let command = `pocketbase/pocketbase${fileExtension} serve --http=${http}`;
+	const pocketbaseExecutable = path.join('pocketbase', 'pocketbase');
+	const pocketbaseArguments = ['serve', `--http=${http}`];
+
 	if (dir) {
-		command += ` --dir=${dir}`;
+		pocketbaseArguments.push(`--dir=${dir}`);
 	}
 	if (migrationsDir) {
-		command += ` --migrationsDir=${migrationsDir}`;
+		pocketbaseArguments.push(`--migrationsDir=${migrationsDir}`);
 	}
 
 	console.info('-> Starting Pocketbase');
-	const pocketBaseServe = spawn(command, { stdio: 'inherit', shell: true });
+	const pocketBaseServe = spawn(pocketbaseExecutable, pocketbaseArguments, {
+		stdio: 'inherit',
+		shell: true
+	});
 
 	pocketBaseServe.on('error', (error) => {
 		console.error(`Error running PocketBase serve: ${error}`);
