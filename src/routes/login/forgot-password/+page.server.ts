8ee107f97fa34adbc7917702c8d9e43200b443b2
Fernando Maclen <fmaclen@gmail.com>
2023-02-28 13:13:15 -0500

    feat: redirect away from guest-only paths

--- src/routes/login/forgot-password/+page.server.ts
+++ src/routes/login/forgot-password/+page.server.ts
@@ -1,7 +1,13 @@
 import { handlePocketbaseErrors } from '$lib/pocketbase.server';
 import { logEventToSlack } from '$lib/slack.server';
+import { redirect } from '@sveltejs/kit';
 
+import type { PageServerLoad } from '../$types';
 import type { Actions } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (locals.pb.authStore.isValid) throw redirect(303, '/');
+};
 
 export const actions: Actions = {
 	default: async ({ request, locals }) => {
