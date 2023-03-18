801ddf87b75e2d59127c835b34f948cfd8576bc0
Fernando Maclen <fmaclen@gmail.com>
2023-03-18 11:00:37 -0400

    wip: remove SYSTEM prompt filter for Messages
    We shouldn't save SYSTEM prompts in the DB in the first place

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -16,26 +16,23 @@ export interface Message {
 	content: ArticleCompletion | string;
 }
 
-// Filters out messages that shouldn't be displayed in the UI, for example: the SYSTEM's prompt
+// Filters out messages that shouldn't be displayed in the UI
 export function getMessages(messagesCollection?: MessageCollection[]): Message[] {
 	if (!messagesCollection) return [];
 
 	const filteredMessages: Message[] = [];
 
-  // Loop through each message in the collection
-  for (const message of messagesCollection) {
-    // Skip messages with the role SYSTEM
-    if (message.role !== MessageRole.SYSTEM) {
-      // Add the message to the filtered messages array
-      filteredMessages.push({
-        id: message.id,
-        updated: message.updated,
-        created: message.created,
-        role: message.role,
-        content: message.content,
-      });
-    }
-  }
+	// Loop through each message in the collection
+	for (const message of messagesCollection) {
+		// Add the message to the filtered messages array
+		filteredMessages.push({
+			id: message.id,
+			updated: message.updated,
+			created: message.created,
+			role: message.role,
+			content: message.content
+		});
+	}
 
 	return filteredMessages.length ? filteredMessages : [];
 }
